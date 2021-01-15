2b8a431bac2913dad82c9084b76efdf46cd4ee5d
Tycho Hartingsveld <70889437+99062718@users.noreply.github.com>
2021-01-15 02:26:32 +0100

    push

---
+++ script.js
@@ -0,0 +1,49 @@
+var maxHealth = 10;
+var currentHealth = 10;
+var damageMulti = 1;
+var chosenDiff;
+var operatorsAvailable = 4;
+
+function diffDamage(){
+	if(chosenDiff == "easy"){
+		currentHealth - 2;
+	}else if(chosenDiff == "medium"){
+		currentHealth - 5;
+	}else if(chosenDiff == "hard"){
+		currentHealth - currentHealth;
+	}
+}
+
+function questionCreator(){
+	switch(chosenDiff){
+		case "easy":
+			return Math.ceil(Math.random() * 50);
+			break;
+		case "medium":
+			return Math.ceil(Math.random() * 200);
+			break;
+		case "hard":
+			return hardQuestion();
+	}
+}
+
+function hardQuestion(){
+	var number1 = Math.ceil(Math.random() * 1000);
+	var number2 = Math.ceil(Math.random() * 1000);
+	var operator = Math.floor(Math.random() * operatorsAvailable);
+
+	switch(operator){
+		case 0:
+			return [number1 + " + " + number2, number1 + number2];
+			break;
+		case 1:
+			return [number1 + " - " + number2, number1 - number2];
+			break;
+		case 2:
+			return [number1 + " * " + number2, number1 * number2];
+			break;
+		case 3:
+			return [number1 + " / " + number2, Math.ceil(number1 / number2)];
+			break;
+	}
+}
